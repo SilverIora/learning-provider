11a3610c9d9572374a34216fc82b20e5ba1d615e
SilverIora <simaycancan888@gmail.com>
2023-07-10 10:41:48 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider_project/provider-page.dart';
+import 'package:provider_project/provider_page.dart';
 
 void main() {
   runApp(const MyApp());
